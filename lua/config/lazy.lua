c4ec55f914fd4fbc6163cdffe53506267261eb00
Trist Truong <nguyentruong@Trists-MacBook-Air.local>
2024-12-22 12:53:17 -0400

    Upgrade plugins and discipline on vim motion

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -55,6 +55,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
+  -- todo tokyonight may get duped here
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
