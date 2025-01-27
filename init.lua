afd9818a9ed72d75220e4feebdf4ad3247ab53a7
Luzushan <trist.truong@uwaterloo.ca>
2025-01-26 20:45:09 -0500

    Retouch ui.lua snack

--- init.lua
+++ init.lua
@@ -13,3 +13,21 @@ require("config.lazy")
 -- bufferline setup
 vim.opt.termguicolors = true
 require("bufferline").setup({})
+
+-- treesitter setup
+-- require("lazy").setup({
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     build = ":TSUpdate",
+--     config = function()
+--       local configs = require("nvim-treesitter.configs")
+--
+--       configs.setup({
+--         ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+--         sync_install = false,
+--         highlight = { enable = true },
+--         indent = { enable = true },
+--       })
+--     end,
+--   },
+-- })
